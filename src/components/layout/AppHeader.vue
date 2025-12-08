6a629bae2a4bda2b6bd87285d9087627622266ac
tuandevdt <doanthanhtuan1998@gmail.com>
2025-12-08 23:27:07 +0700

    hidden notification

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -76,7 +76,7 @@
       >
         <div class="flex items-center gap-2 2xsm:gap-3">
           <!-- <ThemeToggler /> -->
-          <NotificationMenu />
+          <!-- <NotificationMenu /> -->
         </div>
         <UserMenu />
       </div>
@@ -90,7 +90,7 @@ import { useSidebar } from '@/composables/useSidebar'
 // import ThemeToggler from '../common/ThemeToggler.vue'
 // import SearchBar from './header/SearchBar.vue'
 import HeaderLogo from './header/HeaderLogo.vue'
-import NotificationMenu from './header/NotificationMenu.vue'
+// import NotificationMenu from './header/NotificationMenu.vue'
 import UserMenu from './header/UserMenu.vue'
 
 const { toggleSidebar, toggleMobileSidebar, isMobileOpen } = useSidebar()
