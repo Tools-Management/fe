720c7278737e5cf4b07e70034ccdf71350b6d4ad
tuandevdt <doanthanhtuan1998@gmail.com>
2025-12-29 22:43:22 +0700

    feature: add set role for user by admin

--- src/store/user.store.ts
+++ src/store/user.store.ts
@@ -1,5 +1,12 @@
 import { userService } from '@/services/user.service'
-import type { User, AddMoneyRequest, AddMoneyResponse, ChangePasswordRequest, ChangePasswordResponse } from '@/types/user'
+import type {
+  User,
+  AddMoneyRequest,
+  AddMoneyResponse,
+  ChangePasswordRequest,
+  ChangePasswordResponse,
+  UpdateRoleRequest,
+} from '@/types/user'
 import { ResponseError } from '@/utils/error'
 import { defineStore } from 'pinia'
 
@@ -15,6 +22,7 @@ interface UserActions {
   getUserById: (id: string) => Promise<void>
   updateUser: (user: User) => Promise<void>
   updateUserAvatar: (file: File) => Promise<void>
+  updateRoleByAdmin: (request: UpdateRoleRequest) => Promise<void>
   deleteUser: (id: string) => Promise<void>
   addMoneyToWallet: (userId: string, data: AddMoneyRequest) => Promise<AddMoneyResponse | null>
   changePassword: (data: ChangePasswordRequest) => Promise<ChangePasswordResponse | null>
@@ -100,6 +108,23 @@ export const useUserStore = defineStore<'user', UserState, Record<string, never>
         }
       },
 
+      async updateRoleByAdmin(request: UpdateRoleRequest) {
+        this.loading = true
+        this.error = ''
+
+        try {
+          const response = await userService.updateRoleByAdmin(request)          
+
+          if (response instanceof ResponseError){
+            throw response
+          }
+        } catch (error) {
+          this.error = error instanceof Error ? error.message : 'An unexpected error occurred.'
+        } finally {
+          this.loading = false
+        }
+      },
+
       async deleteUser(id: string) {
         this.loading = true
         this.error = ''
@@ -117,7 +142,10 @@ export const useUserStore = defineStore<'user', UserState, Record<string, never>
         }
       },
 
-      async addMoneyToWallet(userId: string, data: AddMoneyRequest): Promise<AddMoneyResponse | null> {
+      async addMoneyToWallet(
+        userId: string,
+        data: AddMoneyRequest,
+      ): Promise<AddMoneyResponse | null> {
         this.loading = true
         this.error = ''
 
@@ -149,7 +177,7 @@ export const useUserStore = defineStore<'user', UserState, Record<string, never>
 
           return response.data as ChangePasswordResponse
         } catch (error) {
-          this.error = error instanceof Error ? error.message : 'Failed to change password.'
+          this.error = error instanceof Error ? error.message : 'Failed to change pasrolesword.'
           return null
         } finally {
           this.loading = false
